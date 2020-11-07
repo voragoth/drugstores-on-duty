d1413f5aee746f701c367a8fcbc946d52a530442
Manuel J. Vasquez Cruz <vcruz.mj@gmail.com>
2020-11-07 15:41:44 -0300

    initial commit, generated from spring initializr

---
+++ src/main/java/com/github/voragoth/drugstores/DrugstoresOnDutyApplication.java
@@ -0,0 +1,13 @@
+package com.github.voragoth.drugstores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugstoresOnDutyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugstoresOnDutyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/voragoth/drugstores/DrugstoresOnDutyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.voragoth.drugstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugstoresOnDutyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
