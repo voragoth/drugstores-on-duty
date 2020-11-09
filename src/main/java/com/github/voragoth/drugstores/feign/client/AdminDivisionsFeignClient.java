7c5fc1cc8e83ba4aadad6a621aa53da414c72324
Manuel J. Vasquez Cruz <vcruz.mj@gmail.com>
2020-11-08 23:13:18 -0300

    fix sonar unused imports before continue

--- src/main/java/com/github/voragoth/drugstores/feign/client/AdminDivisionsFeignClient.java
+++ src/main/java/com/github/voragoth/drugstores/feign/client/AdminDivisionsFeignClient.java
@@ -6,8 +6,6 @@ import org.jsoup.nodes.Document;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
